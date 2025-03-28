c8f1037185964a88ab342f36271c188d9e8070a8
acke7 <ahmadaloalbi@ouylook.com>
2025-03-28 21:49:54 +0100

    Add CustomerViewModel, DTO, and Service implementation
    Introduced a new `CustomerViewModel` class with `Id` and `Name` properties. Added `CustomerDTO` class with `CustomerId` and `CustomerName` properties. Implemented `CustomerService` with a `GetCustomers()` method to retrieve and map customer data. Defined `ICustomerService` interface for service contract. Note: potential mapping issue with `Givenname`.

---
+++ Services/Customers/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer.DTOs;
+using DataAccessLayer.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly BankAppDataContext _dbContext;
+
+        public CustomerService(BankAppDataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<CustomerDTO> GetCustomers()
+        {
+            return _dbContext.Customers.
+                Select(c => new CustomerDTO
+                {
+                    CustomerId = c.CustomerId,
+                    Givenname = c.Givenname
+                }).
+                ToList();
+        }
+    }
+}
---
+++ Services/Customers/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Models;
+using DataAccessLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface ICustomerService
+    {
+        List<CustomerDTO> GetCustomers();
+
+    }
+}
