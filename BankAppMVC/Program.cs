cfa6c134214e3fb039268f1642966fce50e93951
acke7 <ahmadaloalbi@ouylook.com>
2025-03-30 00:06:16 +0100

    Enhance UI, update Bootstrap, and improve jQuery validation
    This commit includes significant updates to the login interface in `Login.cshtml`, improving accessibility and user experience. The shared layout is now consistently applied across views, and the authentication logic has been updated for better user management.
    
    Bootstrap has been upgraded to version 5.3.3, introducing new utility classes, RTL support, and enhancements to the grid system. The JavaScript bundle and ES module have been updated to include the latest components and functionalities.
    
    Additionally, the jQuery Validation Plugin has been updated to version 1.21.0, adding new validation methods and improving AJAX handling. Licensing information has been standardized across several files to ensure compliance.
    
    Overall, these changes enhance the application's usability, responsiveness, and maintainability.

--- BankAppMVC/Program.cs
+++ BankAppMVC/Program.cs
@@ -48,6 +48,7 @@ namespace BankAppMVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
             app.UseRouting();
--- BankAppMVC/ViewModels/CustomerViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankTemplate.ViewModels
-{
-    public class CustomerViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
