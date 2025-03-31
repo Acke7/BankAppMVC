b2f172d29fcda064e5bc28f5365979e5d389321c
acke7 <ahmadaloalbi@ouylook.com>
2025-03-31 23:08:05 +0200

    Enhance registration and user interface components
    - Restructured `Register.cshtml` for improved layout and styling, including Bootstrap integration and role selection enhancements.
    - Added authorization attribute to `RegisterModel` in `Register.cshtml.cs` to restrict access to Admin users.
    - Updated `RegisterConfirmation.cshtml` with a card layout for better user experience.
    - Modified `Index.cshtml` to greet signed-in users and reflect bank activity.
    - Improved `_Layout.cshtml` with new navigation links, search bar, and theme background.
    - Updated `_LoginPartial.cshtml` to conditionally display registration link for Admin users.
    - Enhanced `site.css` with background images for a visually appealing interface.

--- BankAppMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BankAppMVC/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -21,6 +21,8 @@ using Microsoft.Extensions.Logging;
 
 namespace BankAppMVC.Areas.Identity.Pages.Account
 {
+    [Authorize(Roles = "Admin")]
+
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
