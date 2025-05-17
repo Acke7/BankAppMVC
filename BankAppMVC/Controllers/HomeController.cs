b18a1f06cdccfb3920b2467a039524b9147d8e70
acke7 <ahmadaloalbi@ouylook.com>
2025-05-17 17:25:38 +0200

    Enhance HomeController and improve UI components
    Updated HomeController to include ILogger for logging, restructured methods for clarity, and improved maintainability. Cleaned up navigation in _Layout.cshtml by removing unused code and refining dropdown menus. Modified _LoginPartial.cshtml to conditionally render the logout button based on the current page, enhancing user experience. Overall, these changes improve functionality, readability, and user interaction.

--- BankAppMVC/Controllers/HomeController.cs
+++ BankAppMVC/Controllers/HomeController.cs
@@ -1,55 +1,57 @@
-using System.Diagnostics;
-using BankAppMVC.Models;
-using BankAppMVC.Models.ViewModels;
-using BankAppMVC.Models.ViewModels.CustomerVm;
-using Microsoft.AspNetCore.Mvc;
-using Services.Statistics;
+    using System.Diagnostics;
+    using BankAppMVC.Models;
+    using BankAppMVC.Models.ViewModels;
+    using BankAppMVC.Models.ViewModels.CustomerVm;
+    using Microsoft.AspNetCore.Mvc;
+    using Services.Statistics;
 
-namespace BankAppMVC.Controllers
-{
-    public class HomeController : Controller
+    namespace BankAppMVC.Controllers
     {
-        private readonly IStatisticsService _statisticsService;
-
-        public HomeController(IStatisticsService statisticsService)
+        public class HomeController : Controller
         {
-            _statisticsService = statisticsService;
-        }
-
-        [ResponseCache(Duration = 60, VaryByQueryKeys = new[] { "country" })]
-        [HttpGet("/TopCustomers/{country}")]
-        public async Task<IActionResult> TopCustomers(string country)
-        {
-            var dtoList = await _statisticsService.GetTopCustomersByCountryAsync(country);
-            var viewModels = dtoList.Select(x => new TopCustomerViewModel
+            private readonly IStatisticsService _statisticsService;
+            private readonly ILogger<HomeController> _logger;
+            public HomeController(IStatisticsService statisticsService, ILogger<HomeController> logger)
             {
-                CustomerId = x.CustomerId,
-                Givenname = x.Givenname,
-                Surname = x.Surname,
-                TotalBalance = x.TotalBalance
-            }).ToList();
-
-            ViewBag.Country = country;
-            return View(viewModels);
-        }
-
-
-        public async Task< IActionResult >Index()
-        {
-            var swedenTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
-            var swedenTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, swedenTimeZone);
-            ViewBag.SwedenTime = swedenTime; ;
-            var dtoList = await _statisticsService.GetCountryStatistics();
-
-            var viewModelList = dtoList.Select(dto => new CountryStatsticsViewModel
+                _statisticsService = statisticsService;
+                _logger = logger;
+            }
+            [IgnoreAntiforgeryToken]
+            [ResponseCache(Duration = 60, VaryByQueryKeys = new[] { "country" })]
+            [HttpGet("/TopCustomers/{country}")]
+            public async Task<IActionResult> TopCustomers(string country)
             {
-                Country = dto.Country,
-                Customers = dto.CustomerCount,
-                Accounts = dto.AccountCount,
-                TotalSaldo = dto.TotalBalance
-            }).ToList();
+                _logger.LogInformation($"TopCustomers action HIT for {country} at {DateTime.Now}");
+                var dtoList = await _statisticsService.GetTopCustomersByCountryAsync(country);
+                var viewModels = dtoList.Select(x => new TopCustomerViewModel
+                {
+                    CustomerId = x.CustomerId,
+                    Givenname = x.Givenname,
+                    Surname = x.Surname,
+                    TotalBalance = x.TotalBalance
+                }).ToList();
 
-            return View(viewModelList);
+                ViewBag.Country = country;
+                return View(viewModels);
+            }
+
+
+            public async Task< IActionResult >Index()
+            {
+                var swedenTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+                var swedenTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, swedenTimeZone);
+                ViewBag.SwedenTime = swedenTime; ;
+                var dtoList = await _statisticsService.GetCountryStatistics();
+
+                var viewModelList = dtoList.Select(dto => new CountryStatsticsViewModel
+                {
+                    Country = dto.Country,
+                    Customers = dto.CustomerCount,
+                    Accounts = dto.AccountCount,
+                    TotalSaldo = dto.TotalBalance
+                }).ToList();
+
+                return View(viewModelList);
+            }
         }
     }
-}
